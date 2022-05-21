85d2f11df4a6fae1132dcccb3aa154da44aa228b
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2022-05-21 08:26:55 +0300

    Update initialization.py

--- initialization.py
+++ initialization.py
@@ -1,11 +1,13 @@
-# initialize and populate all_walks
+# initialization
 import numpy as np
 import matplotlib as plt
+
+
 all_walks = []
 for i in range(10) :
-    random_walk = [0]
+    random_walks = [0]
     for x in range(100) :
-        step = random_walk[-1]
+        step = random_walks[-1]
         dice = np.random.randint(1,7)
         if dice <= 2:
             step = max(0, step - 1)
@@ -13,22 +15,22 @@ for i in range(10) :
             step = step + 1
         else:
             step = step + np.random.randint(1,7)
-        random_walk.append(step)
-    all_walks.append(random_walk)
+        random_walks.append(step)
+    all_walk2.append(random_walks)
 
-# Convert all_walks to NumPy array: np_aw
-np_aw = np.array(all_walks)
+# converting to the NumPy array
+np_aw = np.array(all_walk2)
 
-# Plot np_aw and show
+# showing of np_aw
 plt.plot(np_aw)
 plt.show()
 
-# Clear the figure
+# Clearing
 plt.clf()
 
-# Transpose np_aw: np_aw_t
-np_aw_t = np.transpose(np_aw)
+# transporting to the np_aw_transported
+np_aw_transported = np.transpose(np_aw)
 
-# Plot np_aw_t and show
-plt.plot(np_aw_t)
+# Plot of np_aw_transported and show
+plt.plot(np_aw_transported)
 plt.show()
